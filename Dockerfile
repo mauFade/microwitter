b0e63ce89cfdc6f4094d678022258da166b1dbc9
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-09-05 22:03:51 -0300

    first :rocket:

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:latest
+WORKDIR ./app
+COPY ./package.json ./
+RUN npm i
+COPY ./ ./
+RUN npm run build
+CMD ["npm", "start"]
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import { router } from "./routes";
+
+const app: Application = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(router);
+
+export { app };
